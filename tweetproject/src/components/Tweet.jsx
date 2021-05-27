2460fb1bb8a3ceddc09d415d6b01cbe1554db331
uttej-vsk <uttejvsk@hotmail.com>
2021-05-28 00:09:28 +0530

    dynamic update of tweets based on object data

--- tweetproject/src/components/Tweet.jsx
+++ tweetproject/src/components/Tweet.jsx
@@ -4,17 +4,17 @@ import React from "react";
 const Tweet = props =>{
     return(
         <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{props.twitterhandle}</h6>
-                <p className="card-text">{props.tweet}</p>
+            {<div className="card-body">
+                <h5 className="card-title">{props.tweet.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{props.tweet.handle}</h6>
+                <p className="card-text">{props.tweet.tweet}</p>
                 <a href="" className="card-link">
-                    {props.phonenumber}
+                    {props.tweet.phonenumber}
                 </a>
                 <a href="" className="card-link">
-                    {props.email}
+                    {props.tweet.email}
                 </a>
-            </div>
+            </div>}
         </div>
     )
 }
--- tweetproject/src/components/Tweets.jsx
+++ tweetproject/src/components/Tweets.jsx
@@ -2,17 +2,38 @@ import React from "react";
 import Tweet from "./Tweet";
 
 const Tweets = () => {
-  return (
-    <div>
-      <Tweet
-        name="Uttej"
-        email="uttejhearts@gmail.com"
-        twitterhandle="@1Uttej"
-        phonenumber="9705859153"
-        tweet="this is my new tweet"
-      />
-    </div>
-  );
+  const tweets = [
+    {
+      name: "Uttej",
+      handle: "@1Uttej",
+      tweet: "hi from backend",
+      phonenumber: "9705859153",
+      email: "uttejhearts@gmail.com",
+    },
+
+    {
+      name: "Uttej Venkata ",
+      handle: "@1Uttejvenkata",
+      tweet: "hi from backend from venkata",
+      phonenumber: "9705859153111",
+      email: "uttejvenkata@gmail.com",
+    },
+
+    {
+      name: "Uttejsastry",
+      handle: "@1Uttej",
+      tweet: "hi from backend 3",
+      phonenumber: "9705859153 2344",
+      email: "uttejsastry@gmail.com",
+    },
+  ];
+
+  const renderTweets = () => {
+    return tweets.map((el) => {
+      return <Tweet tweet={el} />;
+    });
+  };
+  return <div>{renderTweets()}</div>;
 };
 
 export default Tweets;
